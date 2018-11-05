b6b61cd8eca733360ae76f96b9c9938f96ed14a3
BuildTools <unconfigured@null.spigotmc.org>
2018-11-06 00:41:20 +0900

    commit test

--- app/src/main/java/com/example/kmchs/listviewthread/listviewthreadv01/MainActivity.java
+++ app/src/main/java/com/example/kmchs/listviewthread/listviewthreadv01/MainActivity.java
@@ -74,6 +74,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //commit test
 
         SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
         dataSaver= sharedPref.getBoolean("thumbnail",true);

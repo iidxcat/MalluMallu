91f8158165dbd66aa820cdde4b05efc02b9e84aa
cat <johnson740394@gmail.com>
2018-11-11 15:45:15 +0900

    history clear added

--- app/src/main/java/com/example/kmchs/listviewthread/listviewthreadv01/SettingPreferenceFragment.java
+++ app/src/main/java/com/example/kmchs/listviewthread/listviewthreadv01/SettingPreferenceFragment.java
@@ -1,7 +1,12 @@
 package com.example.kmchs.listviewthread.listviewthreadv01;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.util.Log;
+import android.widget.Toast;
 
 public class SettingPreferenceFragment extends PreferenceFragment {
     @Override
@@ -10,5 +15,20 @@ public class SettingPreferenceFragment extends PreferenceFragment {
 
         // Load the preferences from an XML resource
         addPreferencesFromResource(R.xml.settings_preference);
+        Preference myPref=(Preference) findPreference("clearHistory");
+        myPref.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            public boolean onPreferenceClick(Preference preference) {
+                SharedPreferences pref1;
+                for(int i=1; i<33; i++) {
+                    Context context=getActivity();
+                    pref1 = context.getSharedPreferences("pref"+String.valueOf(i), Context.MODE_PRIVATE);
+                    SharedPreferences.Editor editor=pref1.edit();
+                    editor.putString("title", null);
+                    editor.commit();
+                }
+                Toast.makeText(getActivity(), "삭제 되었습니다.", Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
     }
 }
